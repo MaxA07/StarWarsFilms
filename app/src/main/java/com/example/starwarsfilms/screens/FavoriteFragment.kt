8861c6f5b76c53c99500b249a0ef609be060013d
MaxA07 <amorfis77@gmail.com>
2022-09-05 17:55:24 +0300

    add bottom navigation

---
+++ app/src/main/java/com/example/starwarsfilms/screens/FavoriteFragment.kt
@@ -0,0 +1,27 @@
+package com.example.starwarsfilms.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.starwarsfilms.R
+
+class FavoriteFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = FavoriteFragment()
+
+    }
+}
